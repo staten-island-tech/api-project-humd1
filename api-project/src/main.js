0f88d728e8fe6fcd575646de6a57dd948b231b93
humd1 <humdb@nycstudents.net>
2025-12-17 12:11:30 -0500

    ljd

--- api-project/src/main.js
+++ api-project/src/main.js
@@ -3,22 +3,26 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+async function getData(URL) {
+  try {
+    //go get data
+    const response = await fetch(URL);
+    //handle errors
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //makes the response into json data we can use
+      const data = await response.json(); //makes the data into JSON object we can use
+      console.log(data);
+      document.getElementById("api-response").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("no bueno");
+  }
+}
 
-setupCounter(document.querySelector('#counter'))
+const URL = "https://makeup-api.herokuapp.com/api/v1/products.json";
+getData(URL);
+
+console.log(data);
