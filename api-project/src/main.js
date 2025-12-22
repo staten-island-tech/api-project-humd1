b1cef6440909731a1e03ec3ed4ff41353244cedf
humd1 <humdb@nycstudents.net>
2025-12-22 12:04:35 -0500

    daisyui

--- api-project/src/main.js
+++ api-project/src/main.js
@@ -3,26 +3,21 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+//get yo data//
 async function getData(URL) {
   try {
-    //go get data
     const response = await fetch(URL);
-    //handle errors
-    if (response.status != 200) {
-      throw new Error(response);
-    } else {
-      //makes the response into json data we can use
-      const data = await response.json(); //makes the data into JSON object we can use
-      console.log(data);
-      document.getElementById("api-response").textContent = data.name;
-    }
+    if (response.status !== 200) throw new Error(response);
+    const data = await response.json();
+    console.log(data);
+    document.getElementById("api-response").textContent = data[0]?.name || "No data found";
+    return data;
   } catch (error) {
     console.log(error);
     console.log("no bueno");
   }
 }
-
 const URL = "https://makeup-api.herokuapp.com/api/v1/products.json";
-getData(URL);
+getData(URL).then(data => console.log(data));
 
-console.log(data);
+
